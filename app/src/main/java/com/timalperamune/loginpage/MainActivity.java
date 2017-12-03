0ce333c90c146a6c1ae5298472283145f9e4cc2c
tperamun <tperamun@uwo.ca>
2017-12-03 00:41:09 -0500

    SecontActivity.java added

--- app/src/main/java/com/timalperamune/loginpage/MainActivity.java
+++ app/src/main/java/com/timalperamune/loginpage/MainActivity.java
@@ -1,8 +1,11 @@
 package com.timalperamune.loginpage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+
+        Button login_button = (Button) findViewById(R.id.button);
+
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, SecondActivity.class);
+//
+                startActivity(myIntent);
+
+            }
+            });
+
+
     }
+
 }
